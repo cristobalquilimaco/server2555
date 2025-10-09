245340eccab87197e07a97d88d61aa7438e8db74
Cristobal <quilimacox1@gmail.com>
2025-10-09 10:39:17 -0500

    Se crear nuevo archivo iconsPartners.ts para manejo de los iconos de los partners

---
+++ src/utils/iconsPartners.ts
@@ -0,0 +1,16 @@
+// Importa imágenes desde la carpeta src o assets, ejemplo:
+import awsIcon from '../images/datacenter/aws.png';
+import googleCloudIcon from '../images/datacenter/Google_Cloud_logo.png';
+import ibmIcon from '../images/datacenter/IBM_logo.svg.png';
+import nvidiaIcon from '../images/datacenter/NVIDIA_logo.svg.png';
+
+// Exporta arreglo de partners con nombre y icono importado
+export const partners = [
+  { name: 'AWS', icon: awsIcon },
+  { name: 'Google Cloud', icon: googleCloudIcon },
+  { name: 'IBM', icon: ibmIcon },
+  { name: 'NVIDIA', icon: nvidiaIcon },
+  { name: 'AWS', icon: awsIcon },
+  { name: 'Google Cloud', icon: googleCloudIcon }
+];
+
